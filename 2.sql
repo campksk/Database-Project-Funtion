dccb640e33f8cd497606fdd4fce0d9ac5041046f
camp_.ksk <camp131249@gmail.com>
2026-02-03 14:33:06 +0700

    Create 2.sql

---
+++ 2.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION LoginUser(p_Email VARCHAR, p_Pass VARCHAR)
+RETURNS TABLE(UserId INT, FName VARCHAR, LName VARCHAR) 
+LANGUAGE plpgsql AS $$
+BEGIN
+    RETURN QUERY 
+    SELECT a.UserId, a.FName, a.LName 
+    FROM Account a
+    WHERE a.Email = p_Email AND a.Password = p_Pass;
+END;
+$$;
+
+SELECT * FROM LoginUser('john@email.com', 'mypassword');
